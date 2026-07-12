e46123930ce851c5c86403e2ff4416331338e09f
Seolhwa <devseolhwa@gmail.com>
2026-07-12 23:17:41 +0900

    cccsssdd

--- assets/js/sub.js
+++ assets/js/sub.js
@@ -1,3 +1,29 @@
 $(function(){
+    // 요소접근 이벤트
+    setTimeout(function(){
+        showLayer();
+    }, 2000);
 
+    function showLayer(){
+        $(".showLayer").each(function(){
+            let $this = $(this);
+            let start_pos = "top bottom";
+            let end_pos =  "bottom top";
+
+            ScrollTrigger.create({
+                trigger: $this,
+                start: start_pos, 
+                end: end_pos,
+                onEnter: function(){
+                    $this.addClass("active");
+                },onLeave: function(){
+                    $this.removeClass("active");
+                },onEnterBack: function(){
+                    $this.addClass("active");
+                },onLeaveBack: function(){
+                    $this.removeClass("active");
+                }
+            });
+        });
+    }
 });
