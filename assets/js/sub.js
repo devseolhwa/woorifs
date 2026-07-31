0de06a0a633e2a0cd1b043496acbb08dd0ef9a13
Seolhwa <devseolhwa@gmail.com>
2026-07-31 13:59:44 +0900

    dd

--- assets/js/sub.js
+++ assets/js/sub.js
@@ -85,4 +85,11 @@ $(function(){
 
     scrollBar.init();
 
+    // snb
+	$(".snbList li button").on("click", function(){
+		$(this).parent("li").toggleClass("active").siblings("li").removeClass("active");
+		$(this).next(".depth02").stop().slideToggle();
+		$(this).parents().siblings("li").find(".depth02").slideUp();
+	});
+
 });
